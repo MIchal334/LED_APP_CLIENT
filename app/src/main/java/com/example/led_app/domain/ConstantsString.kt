68368004dac70daddb8b6bc374fcebe1ae29f058
michal.mencfel <michal.mencfel@kitopi.com>
2024-05-29 17:43:26 +0200

    change spelling error

--- app/src/main/java/com/example/led_app/domain/ConstantsString.kt
+++ app/src/main/java/com/example/led_app/domain/ConstantsString.kt
@@ -1,34 +1,32 @@
 package com.example.led_app.domain
 
 object ConstantsString {
-    const val APP_NAME = "LED APP";
-    const val CHOSE_SERVER = "Wybierz LED z kotryem chcesz się połączyć:";
+    const val APP_NAME = "LED APP"
+    const val CHOSE_SERVER = "Wybierz LED, z którym chcesz się połączyć:"
     const val SERVER_NAME = "NAZWA LED:"
     const val DIALOG_TITLE_INFORMATION = "INFORMACJA"
-    const val DIALOG_INFORMATION_LED_NOT_EXIST = "To oswietlenie led nie jest w tej chwili dostępne "
-    const val ADD_NEW_LED = "Dodaj nowe oswietlenie"
+    const val DIALOG_INFORMATION_LED_NOT_EXIST = "To oświetlenie LED nie jest w tej chwili dostępne."
+    const val ADD_NEW_LED = "Dodaj nowe oświetlenie"
     const val LABEL_ADD_LED_NAME = "NAZWA LED"
-    const val LABEL_ADD_LED_IP = "Adress w sieci"
+    const val LABEL_ADD_LED_IP = "Adres w sieci"
     const val BUTTON_ADD_NEW_LED = "Dodaj nowe LED"
-    const val LED_SERVER_NOT_RESPONSE = "LED pod tym adresem nie odpowiada. Możliwe że nie istnieje"
+    const val LED_SERVER_NOT_RESPONSE = "LED pod tym adresem nie odpowiada. Możliwe, że nie istnieje."
     const val BUTTON_SET_NEW_COLOR = "Ustaw nowy kolor"
     const val BUTTON_CHOSE_MODES = "Wybierz tryb oświetlenia"
     const val BUTTON_UPDATE_DATA = "Uaktualnij dane LED"
     const val BUTTON_TURN_OFF_LED = "Wyłącz LED"
-    const val BUTTON_CHOSE_CHANGE_MODE = "TRYB PRZEJSCIA"
+    const val BUTTON_CHOSE_CHANGE_MODE = "TRYB PRZEJŚCIA"
     const val ERROR_OCCURED = "Wystąpił błąd"
     const val LED_TURNED_OFF = "LED WYŁĄCZONY"
-    const val CHANGE_OPTION_NEEDED_TO_CHOOSE = "Wybierz opcje przejscia"
-    const val SEND_REQUEST = "Wyslij rzadanie"
-    const val SEND_ERROR_OCCURED = "Wystąpił błąd podczas wysylania rzadania"
+    const val CHANGE_OPTION_NEEDED_TO_CHOOSE = "Wybierz opcję przejścia"
+    const val SEND_REQUEST = "Wyślij żądanie"
+    const val SEND_ERROR_OCCURED = "Wystąpił błąd podczas wysyłania żądania"
     const val LED_UPDATED = "Dane zaktualizowane pomyślnie"
-    const val LED_DATA_NOT_VALID = "Podane dane są błędne. Nazwa isteje już w systemie lub adres serwera jest nie poprawny"
+    const val LED_DATA_NOT_VALID =
+        "Podane dane są błędne. Nazwa istnieje już w systemie lub adres serwera jest niepoprawny."
     const val LED_DELETED = "LED został usunięty"
-    const val BUTTON_DELETED_LED = "Usuń LED z urządzania"
-    const val DELETE_LED = "Czy na pewno chcesz usunąć ten LED"
+    const val BUTTON_DELETED_LED = "Usuń LED z urządzenia"
+    const val DELETE_LED = "Czy na pewno chcesz usunąć ten LED?"
     const val YES = "Tak"
     const val NO = "Nie"
-
-
-
 }
